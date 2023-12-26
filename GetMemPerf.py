d5a034bd39e0c56a24e76caf3169342d64b6a506
liuweidin <1045251744@qq.com>
2023-12-26 23:10:49 +0800

    add mean loadtouse figure and vecviolation figure

--- GetMemPerf.py
+++ GetMemPerf.py
@@ -24,18 +24,44 @@ def getVecViolation():
     filelist = pd.read_csv('./generates/filelist')
     basePath = './generates/'
     sufix = '.csv'
-    vecViolationNum = []
+    vecViolationNum = np.array([])
     for i in range(filelist.size):
         path = basePath + filelist.loc[i].values + sufix
         df = readOnecsv(path[0])
-        vecViolationNum.append(df[df['Name'] == "system.cpu.lsq0.VecMemOrderViolation"]['Perf_Number'].values)
-    print(vecViolationNum)
-    x = np.array(vecViolationNum)
+        vecViolationNum = np.append(vecViolationNum, df[df['Name'] == "system.cpu.lsq0.VecMemOrderViolation"]['Perf_Number'].values)
+        if(df.size == 0):
+            continue
+        if (df[df['Name'] == "system.cpu.lsq0.VecMemOrderViolation"]['Perf_Number'].values > 100):
+            print(df[df['Name'] == "system.cpu.lsq0.VecMemOrderViolation"])
+    x = vecViolationNum
+    print(x)
     y = range(filelist.size)
-    print(y)
-    plt.hist(x,bins=y,)
+    plt.hist(x,bins=y)
+    plt.show()
+
+def getMeanloadtouse():
+    filelist = pd.read_csv('./generates/filelist')
+    basePath = './generates/'
+    sufix = '.csv'
+    vecMeanLoadtoUse = np.array([])
+    scaleMeanLoadtoUse = np.array([])
+    for i in range(filelist.size):
+        path = basePath + filelist.loc[i].values + sufix
+        df = readOnecsv(path[0])
+        vecMeanLoadtoUse = np.append(vecMeanLoadtoUse, df[df['Name'] == "system.cpu.lsq0.vecLoadToUse::mean"]['Perf_Number'].values)
+        scaleMeanLoadtoUse = np.append(scaleMeanLoadtoUse, df[df['Name'] == "system.cpu.lsq0.scaleLoadToUse::mean"]['Perf_Number'].values)
+    x = vecMeanLoadtoUse
+    print(x)
+    y = range(filelist.size)
+    plt.subplot(2,1,1)
+    plt.hist(x,bins=y)
+    plt.title("vector Load to use")
+
+    plt.subplot(2,1,2)
+    plt.hist(scaleMeanLoadtoUse,bins=y)
+    plt.title("scale Load to use")
     plt.show()
 
 getVecViolation()
-        
+getMeanloadtouse()
 
